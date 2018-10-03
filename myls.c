8aadff2023801b6f7606ef5e33ca2bb5fbf0285c
cbirdsey <cbirdsey@middlebury.edu>
2018-10-02 22:40:26 -0400

    updated handling of file and directory in cmd-line, cleaned up code, commented, and removed use of chdir(). need to refactor test_flags

--- myls.c
+++ myls.c
@@ -1,4 +1,4 @@
-//calder birdsey 
+//calder birdsey
 //cs315
 //assignment 01 
 //9/20/18 
@@ -15,95 +15,19 @@
 #include <pwd.h>
 #include <time.h>
 
-//function to print file/directory data with ls option -1 
-void print_l(struct dirent *dir_entry){
-    struct stat statbuf; 
-    if(stat(dir_entry->d_name, &statbuf) == -1){
-        perror("stat"); 
-        exit(4); 
-    }else{
-        printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-"); 
-        printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
-        printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
-        printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
-        printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
-        printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
-        printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
-        printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
-        printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
-        printf((statbuf.st_mode & S_IXOTH) ? "x " : "- ");
-        printf("%li ", statbuf.st_nlink);
-        struct passwd *pw; 
-        pw = getpwuid(statbuf.st_uid);  
-        printf("%s ", pw->pw_name); 
-        pw = getpwuid(statbuf.st_gid);
-        printf("%s ", pw->pw_name); 
-        printf("%5ld ", statbuf.st_size);
-        struct tm *tmp;
-        char outstr[200];
-        time_t t = statbuf.st_mtime;
-        tmp = localtime(&t);   
-        if(tmp == NULL){
-            perror("localtime"); 
-            exit(EXIT_FAILURE);
-        } 
-        strftime(outstr, sizeof(outstr), "%b %d %R", tmp); 
-        printf("%s ", outstr);
-        printf("%s\n", dir_entry-> d_name); 
-    }
-}
-
-//function to check flags and print file/directory info accordingly
-void flag_test(struct dirent *dir_entry, int flag_a, int flag_l){
-    if(flag_a == 0){
-        if((dir_entry->d_name[0] != '.')){ 
-            if(flag_l == 0){
-                printf("%s ", dir_entry->d_name);
-            }else{
-                print_l(dir_entry);  
-            }
-        }
-    }else{
-        if(flag_l == 0){
-            printf("%s ", dir_entry->d_name);
-        }else{
-            print_l(dir_entry);  
-        }
-    }
-}
-
-//fucntion to check if myls argument is an existing file or directory in cwd 
-void dir_check(int argc, char *argv[]){
-    for(int i = 1; i <= (argc-1); i++){
-        if(argv[i][0] != '-'){
-            struct stat argbuf;
-            char * arg_file = argv[i]; 
-            if((stat(arg_file, &argbuf)) == -1){
-                printf("myls: cannot access '%s'; No such file or directory\n",
-                argv[i]);  
-            }else{
-                if(S_ISDIR(argbuf.st_mode)){
-                    chdir(argv[i]); 
-                }else{
-                    printf("file\n");//unsure how to do equivalent of chdir() for a file 
-                } 
-            }             
-        }
-    }
-}
+extern void print_l(struct dirent *dir_entry); 
+extern void flag_test(struct dirent *dir_entry, int flag_a, int flag_l);
+extern void print_args(char *dir_arg, char *file, int flag_a, int flag_l, int flag_file); 
 
 //main function    
 int main(int argc, char *argv[]){
-    //check command line args 
-    if(argc > 4){
-        printf("usage: %s source dest\n", argv[0]); 
-        exit(EXIT_FAILURE); 
-    }
-    if(argc > 1){
-        dir_check(argc, argv); 
-    }
-    //set flags
-    int flag_a, flag_l = 0;
+    //initialize flags
+    int flag_l = 0; 
+    int flag_a = 0; 
+    int flag_file = 0; //flag if a file is passed on cmd-line
+    int data_arg = 0; //flagged if dir/file args are passed on cmd-line 
+
+    //get options 
     int opt; 
     while((opt = getopt(argc, argv, "al")) != -1) {
         switch (opt) {
@@ -118,26 +42,120 @@ int main(int argc, char *argv[]){
             exit(EXIT_FAILURE); 
         }
     }
+
+    //check command line args and print accordingly 
+    if(argc > 1){
+        for(int i = 1; i <= (argc-1); i++){ 
+            if(argv[i][0] != '-'){
+                struct stat argbuf;
+                char *arg = argv[i]; 
+                if((stat(arg, &argbuf)) == -1){
+                    data_arg = 1; 
+                    printf("myls: cannot access '%s': No such file or directory\n", argv[i]);
+                }else{
+                    data_arg = 1;
+                    if(S_ISREG(argbuf.st_mode)){
+                        flag_file = 1;
+                        print_args(".", arg, flag_a, flag_l, flag_file); 
+                    }
+                    if(S_ISDIR(argbuf.st_mode)){
+                        printf("%s:\n", arg); 
+                        print_args(arg, "NULL", flag_a, flag_l, flag_file); 
+                    }
+                    flag_file = 0; //resetting file_flag each iteration through 'for' loop
+                }      
+            }
+        }
+    }
+    if(data_arg == 0){
+        print_args(".", "NULL", flag_a, flag_l, flag_file); //if myls is called with no dir/file args
+    }
+}
+
+//function to print file/directory data with ls option -l
+void print_l(struct dirent *dir_entry){
+    struct stat statbuf; 
+    if(stat(dir_entry->d_name, &statbuf) == -1){
+        perror("stat"); 
+        exit(EXIT_FAILURE); 
+    }
+    printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-"); 
+    printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
+    printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
+    printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
+    printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
+    printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
+    printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
+    printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
+    printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
+    printf((statbuf.st_mode & S_IXOTH) ? "x " : "- ");
+    printf("%li ", statbuf.st_nlink);
+    struct passwd *pw; 
+    pw = getpwuid(statbuf.st_uid);  
+    printf("%s ", pw->pw_name); 
+    pw = getpwuid(statbuf.st_gid);
+    printf("%s ", pw->pw_name); 
+    printf("%5ld ", statbuf.st_size);
+    struct tm *tmp;
+    char outstr[200];
+    time_t t = statbuf.st_mtime;
+    tmp = localtime(&t);   
+    if(tmp == NULL){
+        perror("localtime"); 
+        exit(EXIT_FAILURE);
+    } 
+    strftime(outstr, sizeof(outstr), "%b %d %R", tmp); 
+    printf("%s ", outstr);
+    printf("%s\n", dir_entry->d_name); 
+}
+
+//function to check flags and print file/directory info accordingly
+void flag_test(struct dirent *dir_entry, int flag_a, int flag_l){
+    if(flag_a == 0){
+        if((dir_entry->d_name[0] != '.')){ 
+            if(flag_l == 0){
+                printf("%s ", dir_entry->d_name);
+            }else{
+                print_l(dir_entry);
+            } 
+        }
+    }else{
+        if(flag_l == 0){
+            printf("%s ", dir_entry->d_name);
+        }else
+            print_l(dir_entry);
+    }
+}
+
+void print_args(char *dir_arg, char *file, int flag_a, int flag_l, int flag_file){
     //open directory
-    char cwd[PATH_MAX]; 
-    char *cdir = getcwd(cwd, sizeof(cwd)); 
-    DIR *dir = opendir(cdir);
+    DIR *dir = opendir(dir_arg);
     if(dir == NULL){
         perror("opendir"); 
         exit(EXIT_FAILURE);
     } 
-    //read directory
+
+    //read and print directory/file data 
     struct dirent *dir_entry; 
     rewinddir(dir); 
     errno = 0;
-    while((dir_entry = readdir(dir))!= NULL){
-        flag_test(dir_entry, flag_a, flag_l); 
+    while((dir_entry = readdir(dir))!= NULL){ 
+        if(flag_file == 1){
+            if(strcmp(dir_entry->d_name, file) == 0){ //comparing current file to cmd-line arg
+                flag_test(dir_entry, flag_a, flag_l);
+            }
+        }else{
+            flag_test(dir_entry, flag_a, flag_l); 
+        }
     }
-    printf("\n"); 
+    if(flag_l == 0){
+        printf("\n");
+    }
     if(errno == 1){
         perror("readdir");
         exit(EXIT_FAILURE); 
     }
+
     //close directory
     closedir(dir);
 }
